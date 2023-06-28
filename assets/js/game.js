032937118dc23702156d4cf94dfd92d794b6f9e2
ShaneByrne0963 <>
2023-06-28 20:22:45 +0000

    add: functions for setting and clearing tiles

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,9 @@
+const boardSize = 8;
+
 document.onload = gameInit();
 
 /**
- * Initialises the game
+ * Creates the chess tiles and then starts the game
  */
 function gameInit() {
     //Setting up the chess board tiles
@@ -9,8 +11,8 @@ function gameInit() {
     let chessGrid = ``;
     let isWhite = true;
 
-    for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
+    for (let i = 0; i < boardSize; i++) {
+        for (let j = 0; j < boardSize; j++) {
             let tileClass;
             if (isWhite) {
                 tileClass = 'tile-white';
@@ -18,17 +20,54 @@ function gameInit() {
             else {
                 tileClass = 'tile-black';
             }
+
             chessGrid += `
-            <div id="tile-${j}-${i}" class="tile ${tileClass}">
-                <div class="piece-image"></div>
-            </div>`;
+            <div id="tile-${j}-${i}" class="tile ${tileClass}"></div>`;
 
             //reverses the tile order
             isWhite = !isWhite;
         }
-        //reverses the tile order after each row also
+        //reverses the tile order again after each row
         isWhite = !isWhite;
     }
 
     chessBoard.innerHTML = chessGrid;
+    startGame();
+}
+
+/**
+ * Places the chess pieces in their starting positions on the board
+ */
+function startGame() {
+    for (let i = 0; i < boardSize; i++) {
+        for (let j = 0; j < boardSize; j++) {
+        }
+    }
+}
+
+/**
+ * Sets a chess piece at a given tile
+ * @param {The x position of the tile (from 0 to boardSize - 1)} x 
+ * @param {The y position of the tile (from 0 to boardSize - 1)} y 
+ * @param {The type of piece you wish to set the tile to} piece 
+ * @param {The color of the piece you wish to set the tile to} color 
+ */
+function setTile(x, y, piece, color) {
+    let currentTile = document.getElementById(`tile-${y}-${x}`);
+    let tileClass = currentTile.className;
+    tileClass = tileClass.slice(0, 15); //removes any classes added in the previous game (ends up with "tile tile-white" or "tile tile-black")
+
+    tileClass += ` ${piece} ${color}`;
+    currentTile.className = tileClass;
+}
+
+/**
+ * Clears a given tile of any pieces
+ * @param {The x position of the tile (from 0 to boardSize - 1)} x 
+ * @param {The y position of the tile (from 0 to boardSize - 1)} y 
+ */
+function clearTile(x, y) {
+    let currentTile = document.getElementById(`tile-${y}-${x}`);
+    let tileClass = currentTile.className;
+    tileClass = tileClass.slice(0, 15); //removes any classes added in the previous game (ends up with "tile tile-white" or "tile tile-black")
 }
