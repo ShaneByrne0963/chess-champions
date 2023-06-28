407ae01eaacc651047103bc489e04c48d60e62a6
ShaneByrne0963 <>
2023-06-28 21:01:31 +0000

    add: different chess pieces to chess board

--- assets/js/game.js
+++ assets/js/game.js
@@ -41,20 +41,59 @@ function gameInit() {
 function startGame() {
     for (let i = 0; i < boardSize; i++) {
         for (let j = 0; j < boardSize; j++) {
-            setTile(i, j, 'pawn', 'white');
+            let piece = '';
+            let color = '';
+
+            //creating the back row of chess pieces for each side
+            if (j === 0 || j === boardSize - 1) {
+                //setting the piece type
+                if (i === 0 || i === boardSize - 1) {
+                    piece = 'rook';
+                } else if (i === 1 || i === boardSize - 2) {
+                    piece = 'knight';
+                } else if (i === 2 || i === boardSize - 3) {
+                    piece = 'bishop';
+                } else if (i === 3) {
+                    piece = 'queen';
+                } else {
+                    piece = 'king';
+                }
+
+                //setting the color
+                if (j === 0) {
+                    color = 'black';
+                } else {
+                    color = 'white';
+                }
+                setTile(i, j, piece, color);
+            }
+            //setting up the pawns for both sides
+            else if (j === 1 || j === boardSize - 2) {
+                piece = 'pawn';
+                if (j === 1) {
+                    color = 'black';
+                } else {
+                    color = 'white';
+                }
+                setTile(i, j, piece, color);
+            }
+            //Clearing any tiles set from the previous game
+            else {
+                clearTile(i, j);
+            }
         }
     }
 }
 
 /**
  * Sets a chess piece at a given tile
- * @param {The x position of the tile (from 0 to boardSize - 1)} x 
- * @param {The y position of the tile (from 0 to boardSize - 1)} y 
- * @param {The type of piece you wish to set the tile to} piece 
- * @param {The color of the piece you wish to set the tile to} color 
+ * @param {*The x position of the tile (from 0 to boardSize - 1)*} x 
+ * @param {*The y position of the tile (from 0 to boardSize - 1)*} y 
+ * @param {*The type of piece you wish to set the tile to*} piece 
+ * @param {*The color of the piece you wish to set the tile to*} color 
  */
 function setTile(x, y, piece, color) {
-    let currentTile = document.getElementById(`tile-${y}-${x}`);
+    let currentTile = document.getElementById(`tile-${x}-${y}`);
     let tileClass = currentTile.className;
     tileClass = tileClass.slice(0, 15); //removes any classes added in the previous game (ends up with "tile tile-white" or "tile tile-black")
 
