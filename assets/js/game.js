d7f72d3686d3e10b5b1b79b51222211b4a019646
ShaneByrne0963 <>
2023-06-28 19:19:23 +0000

    add: alternating tile colour pattern for board

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,12 +7,25 @@ function gameInit() {
     //Setting up the chess board tiles
     let chessBoard = document.getElementById("chess-board");
     let chessGrid = ``;
+    let isWhite = true;
 
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
+            let tileClass;
+            if (isWhite) {
+                tileClass = 'tile-white';
+            }
+            else {
+                tileClass = 'tile-black';
+            }
             chessGrid += `
-            <div class="tile tile-white"></div>`;
+            <div class="tile ${tileClass}"></div>`;
+
+            //reverses the tile order
+            isWhite = !isWhite;
         }
+        //reverses the tile order after each row also
+        isWhite = !isWhite;
     }
 
     chessBoard.innerHTML = chessGrid;
